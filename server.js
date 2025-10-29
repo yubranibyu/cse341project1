082186e1fe908f5461e6d3eb048212d3137371e1
yubrani <yag24001@byui.edu>
2025-10-29 01:52:00 -0300

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', require('./routes'));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
